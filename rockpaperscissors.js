4f91fdf4e79bf5d78ba0c315238fda0901845475
Ryan Fincham <ryan@onsitehearing.com>
2021-11-30 15:47:45 -0500

    adding new files to get started

---
+++ rockpaperscissors.js
@@ -0,0 +1,8 @@
+/* Rock Paper Scissors :)
+User imputs a selection of  rock, paper or scissors.
+Comupter randomly selects rock, paper or scissors.
+*/
+function computerPlay () {
+    let
+    return rock OR paper OR scissors;  
+}
